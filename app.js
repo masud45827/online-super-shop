980376a3590c9d6575fe4f6c661a4a6117295dc2
masud45827 <masud.cse2019@gmail.com>
2024-02-28 23:10:08 +0600

    add master branch

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 
 const app = express();
 app.get('/',(req,res)=>{
-    res.send('successful');
+    res.send('successful api');
 })
 app.listen(8000,()=>{
    console.log('data base listening port 8000');

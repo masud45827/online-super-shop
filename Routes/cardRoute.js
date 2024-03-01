56cf4a5f1c9d95f1b7e5329e7ed1dc9aa3ff8401
Fuad7161 <fuadul202@gmail.com>
2024-03-01 17:36:52 +0600

    Product and card added

---
+++ Routes/cardRoute.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const router = express.Router();
+const Card = require('../Modules/cardModel');
+
+router.post('/',async(req,res,next)=>{
+    try{
+        const card = new Card(req.body)
+        const result = await card.save()
+        res.send(result)
+    }catch (error){
+        console.log(error.message);
+    }
+});
+
+router.get('/' , async(req,res,next)=>{
+    try{
+        const result = await Card.find();
+        res.send(result);
+    }catch(error){
+        console.log(error.message);
+    }
+});
+
+module.exports = router;
--- app.js
+++ app.js
@@ -1,12 +1,33 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const bodyPerser = require('body-parser');
 
 const app = express();
+app.use(bodyPerser.json());
+mongoose.connect('mongodb+srv://cluster0.fgp9rxb.mongodb.net/' , {
+    dbName: 'onlineSuperShop',
+    user: 'fuadul202',
+    pass: 'Fuadul11235813',
+}).then(()=>{
+    console.log('Mongodb connected...')
+});
+
+
 app.get('/',(req,res)=>{
     res.send('successful');
 })
 app.post('/',(req,res)=>{
     res.send('successful');
 })
+
+
+const ProductRoute = require('./Routes/productsRoute');
+app.use('/products' , ProductRoute);
+
+
+const Card = require('./Routes/cardRoute');
+app.use('/card',Card);
+
 app.listen(8000,()=>{
    console.log('data base listening port 8000');
 })

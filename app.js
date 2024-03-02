53f4d0772f4709d4ac2d91562d588d1c6c980eb8
Fuad7161 <fuadul202@gmail.com>
2024-03-02 15:25:42 +0600

    passord port user hide.

--- app.js
+++ app.js
@@ -1,13 +1,16 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyPerser = require('body-parser');
+const dotenv = require('dotenv');
+
+dotenv.config();
 
 const app = express();
 app.use(bodyPerser.json());
 mongoose.connect('mongodb+srv://cluster0.fgp9rxb.mongodb.net/' , {
     dbName: 'onlineSuperShop',
     user: 'fuadul202',
-    pass: 'Fuadul11235813',
+    pass: DB_PASSWORD,
 }).then(()=>{
     console.log('Mongodb connected...')
 });
@@ -28,6 +31,6 @@ app.use('/products' , ProductRoute);
 const Card = require('./Routes/cardRoute');
 app.use('/card',Card);
 
-app.listen(8000,()=>{
+app.listen(PORT,()=>{
    console.log('data base listening port 8000');
 })

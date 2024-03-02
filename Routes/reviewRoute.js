928ed9c60ddc5d59be15d1a4f6ea16b317f6236f
Abdul Aziz <azizulcsebsmrstu@gmail.com>
2024-03-02 16:56:59 +0600

    Review image upload

--- Routes/reviewRoute.js
+++ Routes/reviewRoute.js
@@ -1,14 +1,32 @@
 const express = require('express');
 const router = express.Router();
 const Review = require("../Models/reviewModel.js");
+const multer = require('multer');
+const path = require('path');
 
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, './Images/Review') // Destination folder for uploaded files
+    },
+    filename: function (req, file, cb) {
+        let imgUrlName = Date.now() + path.extname(file.originalname);
+        console.log(imgUrlName);
+        cb(null, imgUrlName) // File name
+    }
+});
 
-router.post('', async (req, res)=>{
+// Initialize multer
+const upload = multer({ storage: storage });
+
+router.post('/', upload.single('image'), async (req, res)=>{
     const createReview = new Review(req.body);
+    createReview.image = req.file.filename;
     const result = await createReview.save();
     // console.log("working...");
     // res.send("working...");
-    res.send(createReview);
+    console.log(createReview);
+    res.send("upload Image");
+    // res.send(createReview);
 });
 
 module.exports = router;
--- app.js
+++ app.js
@@ -1,27 +1,37 @@
 const express = require('express');
 const mongoose = require('mongoose');
-const reviewRouter = require('./Routes/review.js');
-const sellRouter = require('./Routes/sell.js');
+const reviewRouter = require('./Routes/reviewRoute.js');
+const sellRouter = require('./Routes/sellRoute.js');
 const bodyParser = require("body-parser");
+require('dotenv').config();
 
 const app = express();
 app.use(bodyParser.json());
 
-mongoose.connect('mongodb+srv://cluster0.fgp9rxb.mongodb.net/' , {
-    dbName: 'onlineSuperShop',
-    user: 'fuadul202',
-    pass: 'Fuadul11235813',
-}).then(()=>{
+mongoose.connect(process.env.MONGO_URL,{})
+.then(()=>{
     console.log('Mongodb connected...')
 });
 
 app.post('/',(req,res)=>{
     console.log("Alhamdulillah....2");
 });
-
 app.use('/api/review', reviewRouter);
 app.use('/api/sell', sellRouter);
 
-app.listen(8000,()=>{
+app.get('/api/review', (req, res)=>{
+    res.render("postReview.ejs");
+});
+
+// app.post('/api/review/upload', async(req, res) => {
+app.post('/api/review', reviewRouter);
+
+    // // app.post('/api/review/upload', async(req, res) => {
+    // app.post('/api/review', upload.single('image'), async(req, res) => {
+    //     console.log(req.file.filename);
+    //     res.send('File uploaded successfully');
+    // });
+
+app.listen(process.env.PORT,()=>{
    console.log('data base listening port 8000');
 })
